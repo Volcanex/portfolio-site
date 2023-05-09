e508313af51577218cea3548e8f0c8af6abd40de
Gabriel Penman <gabrielpenman@gmail.com>
2023-05-09 21:20:53 +0100

    Trying without learning react properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import logo from './assets/logo.svg';
 import styles from './App.module.scss';
 
 function App() {
     return (
         <div className={styles.App}>
             <header className={styles['App-header']}>
-                <img src={logo} className={styles['App-logo']} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={styles['App-link']}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <h1 className={styles.Title}>Gabriel&apos;s playground!</h1>
             </header>
         </div>
     );
