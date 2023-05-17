9a7de4fada82e456f0bf488f11c27ae020e5715c
Gabriel Penman <gabrielpenman@gmail.com>
2023-05-17 18:25:58 +0100

    Trying to fix broken things

--- src/_codux/boards/svg-pan-zoom/svg-pan-zoom.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { SvgPanZoom } from '../../../components/svg-pan-zoom/svg-pan-zoom';
-
-export default createBoard({
-    name: 'SvgPanZoom',
-    Board: () => <SvgPanZoom />
-});
